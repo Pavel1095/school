3083259f9f3bb675db7c7a9d055f7e19af48b86b
Pavel_Gelfer <9153220501@mail.ru>
2024-02-14 20:18:29 +0300

    3.3.done

--- src/main/java/ru/hogwarts/school/service/impl/FacultyServiceImpl.java
+++ src/main/java/ru/hogwarts/school/service/impl/FacultyServiceImpl.java
@@ -3,19 +3,21 @@ package ru.hogwarts.school.service.impl;
 import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.model.Faculty;
-import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.repository.FacultyRepository;
 import ru.hogwarts.school.service.FacultyService;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
 public class FacultyServiceImpl implements FacultyService {
-    private final Map<Long, Faculty> faculties = new HashMap<>();
 
-    private static long currentId = 1;
+    private final FacultyRepository facultyRepository;
+
+    public FacultyServiceImpl(FacultyRepository facultyRepository) {
+        this.facultyRepository = facultyRepository;
+    }
+
 
     @PostConstruct
     public void initFaculties(){
@@ -25,33 +27,32 @@ public class FacultyServiceImpl implements FacultyService {
 
     @Override
     public Faculty add(Faculty faculty) {
-        faculty.setId(currentId++);
-        faculties.put(faculty.getId(), faculty);
-        return faculty;
+      return facultyRepository.save(faculty);
     }
 
     @Override
     public Faculty get(Long id) {
-        return faculties.get(id);
+        return facultyRepository.findById(id).orElse(null);
     }
 
     @Override
     public Faculty update(Long id, Faculty faculty) {
 
-        Faculty facultyFromStorage = faculties.get(id);
-        facultyFromStorage.setName(faculty.getName());
-        facultyFromStorage.setColor(faculty.getColor());
-        return facultyFromStorage;
+        return facultyRepository.findById(id).map(facultyFromDb -> {
+            facultyFromDb.setName(facultyFromDb.getName());
+            facultyFromDb.setColor(faculty.getColor());
+            return facultyRepository.save(facultyFromDb);
+        }).orElse(null);
     }
 
     @Override
-    public Faculty delete(Long id) {
-        return faculties.remove(id);
+    public void delete(Long id) {
+        facultyRepository.deleteById(id);
     }
 
     @Override
     public List<Faculty> getByColor(String color) {
-        return faculties.values()
+        return facultyRepository.findAll()
                 .stream()
                 .filter(it -> it.getColor().equals(color))
                 .collect(Collectors.toList());
---
+++ src/main/java/ru/hogwarts/school/service/impl/StudentServiceImpl.java
@@ -0,0 +1,62 @@
+package ru.hogwarts.school.service.impl;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Service;
+import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.repository.StudentRepository;
+import ru.hogwarts.school.service.StudentService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+
+    private final StudentRepository studentRepository;
+
+    public StudentServiceImpl(StudentRepository studentRepository ) {
+        this.studentRepository = studentRepository;
+    }
+
+
+    @PostConstruct
+    public void initStudents(){
+       if(studentRepository.count() == 0) {
+           add(new Student("Ivan", 25));
+           add(new Student("Petr", 30));
+       }
+    }
+    @Override
+    public Student add(Student student) {
+      return studentRepository.save(student);
+    }
+
+    @Override
+    public Student get(Long id) {
+        return studentRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Student update(Long id, Student student) {
+
+       return studentRepository.findById(id).map(studentFromDb -> {
+            studentFromDb.setName(student.getName());
+            studentFromDb.setAge(student.getAge());
+            return studentRepository.save(studentFromDb);
+        }).orElse(null);
+    }
+
+    @Override
+    public void delete (Long id) {
+        studentRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Student> getByAge(int age) {
+
+        return studentRepository.findAll()
+                .stream()
+                .filter(it -> it.getAge() == age)
+                .collect(Collectors.toList());
+    }
+}
