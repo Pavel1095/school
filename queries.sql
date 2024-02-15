46abbb3160cabaa8f13aaaff40727ab4790f01f9
Pavel_Gelfer <9153220501@mail.ru>
2024-02-15 14:23:52 +0300

    sql

---
+++ queries.sql
@@ -0,0 +1,6 @@
+select * from student;
+select * from student where age > 10 and age < 27;
+select name  from student;
+select * from student where name like '%e%';
+select * from student where age < id;
+select * from student order by age;
